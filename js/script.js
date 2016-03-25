e20846de0522e15be4ec3864b82eee509576a3df
corinne_taylor <corinne.t.clements@gmail.com>
2016-03-24 21:59:03 -0400

    edited footer, fixed script.js
    continued edits to footer, adjusted script.js, finalized layout on visit us page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$(".mainmenu").hide();
+	$("#entrees").show();
 	$(".btn").click(function() {
 		var divId = $(this).attr('data-divid');
 		$('#'+divId).slideToggle();  
